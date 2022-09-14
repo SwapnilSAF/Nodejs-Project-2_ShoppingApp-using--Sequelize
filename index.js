741213b9f6eaca1d626701c1eda66a4731ee0d14
SwapnilSAF <swapnilfulsundar4@gmail.com>
2022-09-14 11:34:54 +0530

    Adding Controllers

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const path = require('path'); //it is a core module in nodejs to provide a path
 
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
+
 //const expressHbs= require("express-handlebars");
 const app = express();
 
-//set template engine pug
+//set template engine hbs
 /*app.engine("hbs", expressHbs({
     layoutsDir: 'views/layouts/',
     defaultlayout: 'main-layout',
@@ -14,18 +17,16 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", "views"); //providing file path 
 
-const adminData = require('./routes/admin'); //importing the adminRoutes
+const adminRoutes = require('./routes/admin'); //importing the adminRoutes
 const shopRoutes = require('./routes/shop'); // importing the shopeRoutes
 
 app.use(bodyParser.urlencoded({extended: false})); //body-parser is which allows express to read the body and 
 //then parse that into a Json object that we can understand. or we can see the user input on console
 app.use(express.static(path.join(__dirname, 'public'))); //for css
 
-app.use('/admin', adminData.routes); //use method allows us to add middleware functions
+app.use('/admin', adminRoutes); //use method allows us to add middleware functions
 app.use(shopRoutes);
 
-app.use((req, res) => {
-    res.status(404).render("404",{pageTitle : 'Page Not Found'});
-});
+app.use(errorController.get404);
 //next allows the request to continue to the next middleware in line
 app.listen(3000);
